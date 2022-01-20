bd4ddf2405d6f3bc382f10365e1a43665c996ebc
KatriKankaanpaa <katri21011@student.hamk.fi>
2022-01-20 14:54:11 +0200

    Lisätty ohjelmointi tehtävä
    10-24.1.2022 tehtävä

---
+++ Tehtävät 10-24.1.2022 vähennys/Tehtavat 10-24.1.2022 vahennys/Tehtavat 10-24.1.2022 vahennys/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tehtavat_10_24._1._2022_vahennys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yhteen- vai vähennyslasku?");
+
+            string vastaus = Console.ReadLine();
+
+            if (vastaus == "+")
+            {
+                Console.WriteLine("Anna luku.");
+                int luku1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Anna toinen luku.");
+                int luku2 = Int32.Parse(Console.ReadLine());
+
+                int yhteen = (luku1 + luku2);
+
+                Console.WriteLine(luku1 + " plus " + luku2 + " on " + yhteen);
+            }
+
+            else if (vastaus == "-")
+            {
+                Console.WriteLine("Anna luku.");
+                int luku1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Anna toinen luku.");
+                int luku2 = Int32.Parse(Console.ReadLine());
+
+                int vahennys = (luku1 - luku2);
+
+                Console.WriteLine(luku1 + " miinus " + luku2 + " on " + vahennys);
+            }
+
+        }
+    }
+}
---
+++ Tehtävät 10-24.1.2022/tehtavat 10-24.1.2022/tehtavat 10-24.1.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tehtavat_10_24._1._2022_summa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Montako lukua?");
+            int vastaus = Int32.Parse(Console.ReadLine());
+
+            int summa = 0;
+
+            for (int i = 0; i < vastaus; i++)
+            {
+                Console.WriteLine("Anna luku.");
+                summa += Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Lukujen keskiarvo on " + summa / vastaus);
+        }
+    }
+}
